c77040bc47d63dc8f8e5c2cc06a844f7a08c3757
96fra <96fra@DESKTOP-8UCB78H>
2018-05-20 18:15:56 +0200

    Soluzione funzionante completa dove faccio tutto in Java.

--- Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
+++ Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
@@ -2,22 +2,103 @@ package it.polito.tdp.dizionariograph.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
 
 public class Model {
+	
+	private WordDAO wordDAO;
+	private List<String> parole;
+	private Graph<String, DefaultEdge> grafo;
+	private String maxDegreeVertex;
+	private int maxDegree;
+	
+	public Model() {
+		this.wordDAO= new WordDAO();
+		this.parole = new ArrayList<>();
+		this.grafo = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+	}
 
 	public void createGraph(int numeroLettere) {
-
-		System.err.println("createGraph -- TODO");
+		
+		this.parole = this.wordDAO.getAllWordsFixedLength(numeroLettere);
+		
+		Graphs.addAllVertices(this.grafo, this.parole);
+		
+		System.out.println("Aggiunti vertici");
+		
+		for(String s : grafo.vertexSet()) {
+			for(String t: grafo.vertexSet()) {
+				if(!s.equals(t)) {
+					int count=0;
+					for(int k =0; k < s.length() && count <=2; k++)
+						if(s.charAt(k) != t.charAt(k))
+							count++;
+					if(count == 1)
+						grafo.addEdge(s, t);
+				}
+			}
+		}
 	}
 
 	public List<String> displayNeighbours(String parolaInserita) {
 
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		/*
+ 		Set<DefaultEdge> edges = this.grafo.outgoingEdgesOf(parolaInserita);
+
+		List<String>res = new ArrayList<>();
+		for(DefaultEdge e: edges)
+			res.add(this.grafo.getEdgeTarget(e));
+		*/
+		List<String> res = Graphs.neighborListOf(this.grafo, parolaInserita);
+		
+		return res;
 	}
 
 	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+		
+		String vertexMax = "_NONE_";
+		int max =0;
+		
+		for(String s : this.grafo.vertexSet()) {
+			int numEdges = this.grafo.degreeOf(s); //edgesOf(s).size();
+			if(numEdges>max) {
+				max = numEdges;
+				vertexMax = s;
+			}
+		}
+		
+		this.maxDegreeVertex = vertexMax;
+		this.maxDegree = max;
+		
+		return max;
 	}
+
+	public Graph<String, DefaultEdge> getGrafo() {
+		return this.grafo;
+	}
+
+	public String getMaxDegreeVertex() {
+		return maxDegreeVertex;
+	}
+
+	public int getMaxDegree() {
+		return maxDegree;
+	}
+
+	public void setMaxDegreeVertex(String maxDegreeVertex) {
+		this.maxDegreeVertex = maxDegreeVertex;
+	}
+
+	public void setMaxDegree(int maxDegree) {
+		this.maxDegree = maxDegree;
+	}
+	
+	
 }
--- Lab08/src/it/polito/tdp/dizionariograph/model/TestModel.java
+++ Lab08/src/it/polito/tdp/dizionariograph/model/TestModel.java
@@ -2,20 +2,53 @@ package it.polito.tdp.dizionariograph.model;
 
 import java.util.List;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
 public class TestModel {
 
 	public static void main(String[] args) {
-		
+	
+	
 		Model model = new Model();
+		
+		/*Graph<String, DefaultEdge> grafo = new SimpleGraph<>(DefaultEdge.class);
+		
+		grafo.addVertex("abaco");
+		grafo.addVertex("abaci");
+		grafo.addVertex("acaci");
+		grafo.addVertex("acini");
+		grafo.addVertex("acino");
+		grafo.addVertex("acine");
+		*/
+//		for(String s : grafo.vertexSet()) {
+//			for(String t: grafo.vertexSet()) {
+//				if(!s.equals(t)) {
+//					
+//					int count=0;
+//					for(int k =0; k < s.length(); k++)
+//						if(s.charAt(k) != t.charAt(k))
+//							count++;
+//					if(count == 1)
+//						grafo.addEdge(s, t);
+//				}
+//			}
+//		}
+//		
 		
 		model.createGraph(4);
 		System.out.println(String.format("**Grafo creato**\n"));
+		System.out.println(model.getGrafo());
 		
-		List<String> vicini = model.displayNeighbours("casa");
+		List<String> vicini =  model.displayNeighbours("casa"); // Graphs.neighborListOf(grafo, "abaco"); 
 		System.out.println("Neighbours di casa: " + vicini + "\n");
 		
 		System.out.println("Cerco il vertice con grado massimo...");
 		System.out.println(model.findMaxDegree());
+		System.out.println(model.getMaxDegreeVertex());
+		
 	}
 
 }
